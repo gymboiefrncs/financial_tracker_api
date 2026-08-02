ee0336382696b7cfbe1151c588ab19b425a27a56
domxzmir <158139549+gymboiefrncs@users.noreply.github.com>
2026-08-02 09:36:36 +0800

    docs: add OpenAPI documentation for user endpoints

--- app/src/main/java/org/financial_tracker/features/user/UserController.java
+++ app/src/main/java/org/financial_tracker/features/user/UserController.java
@@ -11,6 +11,11 @@ import org.financial_tracker.features.user.DTO.CreateUserResponse;
 
 import io.javalin.http.Context;
 import io.javalin.http.HttpStatus;
+import io.javalin.openapi.HttpMethod;
+import io.javalin.openapi.OpenApi;
+import io.javalin.openapi.OpenApiContent;
+import io.javalin.openapi.OpenApiRequestBody;
+import io.javalin.openapi.OpenApiResponse;
 
 public class UserController {
   private final UserService userService;
@@ -19,11 +24,19 @@ public class UserController {
     this.userService = userService;
   }
 
+  @OpenApi(path = "/users/signup", methods = HttpMethod.POST, summary = "Create user", requestBody = @OpenApiRequestBody(content = @OpenApiContent(from = CreateUserRequest.class)), responses = {
+      @OpenApiResponse(status = "201", content = @OpenApiContent(from = CreateUserResponse.class))
+  })
   public void createUser(Context ctx) {
     CreateUserRequest req = ctx.bodyAsClass(CreateUserRequest.class);
     CreateUserResponse user = userService.registerUser(req);
     ctx.status(HttpStatus.CREATED).json(user);
   }
+
+  @OpenApi(path = "/users/login", methods = HttpMethod.POST, summary = "Login user", requestBody = @OpenApiRequestBody(content = @OpenApiContent(from = LoginRequest.class)), responses = {
+      @OpenApiResponse(status = "200", content = @OpenApiContent(from = LoginResponse.class)),
+      @OpenApiResponse(status = "401", description = "Invalid credentials")
+  })
 
   public void loginUser(Context ctx) {
     LoginRequest req = ctx.bodyAsClass(LoginRequest.class);
